1ce8e7b788188afbc91586c994a9df8efae88b54
klefebvre <klefebvre.dev@gmail.com>
2021-07-01 17:16:32 +0200

    V1.1 site mycave

--- add_post.php
+++ add_post.php
@@ -13,11 +13,11 @@ function mb_ucfirst($string)
 }
 
 
-$nom = htmlentities(mb_ucfirst(trim($_POST['nom'])), ENT_QUOTES);
+$nom = htmlentities(mb_strtoupper(trim($_POST['nom'])), ENT_QUOTES);
 $pays = htmlentities(mb_strtoupper(trim($_POST['pays'])), ENT_QUOTES);
 $region = htmlentities(mb_strtoupper(trim($_POST['region'])), ENT_QUOTES);
 $cepage = htmlentities(trim($_POST['cepage']), ENT_QUOTES);
-$annee = htmlentities(trim($_POST['annee']), ENT_QUOTES);
+$annee = intval($_POST['annee']);
 $description = htmlentities(mb_strtoupper(trim($_POST['description'])), ENT_QUOTES);
 $_FILES['image']['name'];
 $_FILES['image']['type'];
@@ -27,6 +27,9 @@ $_FILES['image']['error'];
 $extensions = array('.png', '.gif', '.jpg', '.jpeg');
 $extension = strrchr($_FILES['image']['name'], '.');
 $couleur = intval($_POST['couleur']);
+$array_post = array($nom, $pays, $region, $cepage, $annee, $description, $couleur);
+$array_post_string = array($nom, $pays, $region, $cepage, $couleur);
+
 if (($_FILES['image']['size'] > 0) && ($_FILES['image']['size'] < 1000000) && (in_array($extension, $extensions))) {
 	$image = uniqid() . '.' . $_FILES['image']['name'];
 	$image = strtr(
@@ -42,19 +45,30 @@ if (($_FILES['image']['size'] > 0) && ($_FILES['image']['size'] < 1000000) && (i
 } else {
 	$image = 'generic.jpg';
 }
-$req = $db->prepare('
+
+if (in_array('', $array_post)) {
+	$_SESSION['erreur'] = "Merci de renseigner les champs manquants !";
+} elseif (strlen($_POST['description'])  < 4) {
+	$_SESSION['erreur'] = "La description de la bouteille doit faire plus de 4 caractères";
+} elseif (strlen($_POST['description'])  > 512) {
+	$_SESSION['erreur'] = "La description de la bouteille doit faire moins de 512 caractères";
+} elseif (strlen($_POST['annee'])  < 4) {
+	$_SESSION['erreur'] = "le vin de cette année n'est pas encore récolté";
+} else {
+	$req = $db->prepare('
 					INSERT INTO mille(annee, description,image,couleur_id, nom, pays, region, cepage)
 					VALUES (:annee, :description, :image, :couleur_id, :nom, :pays, :region, :cepage);
 				');
-$req->execute(array(
-	'annee' => $_POST['annee'],
-	'description' => $_POST['description'],
-	'image' => $image,
-	'couleur_id' => $_POST['couleur'],
-	'nom' => $_POST['nom'],
-	'pays' => $_POST['pays'],
-	'region' => $_POST['region'],
-	'cepage' => $_POST['cepage']
-));
-$_SESSION['message'] = "Bouteille ajoutée !";
+	$req->execute(array(
+		'annee' => $_POST['annee'],
+		'description' => $_POST['description'],
+		'image' => $image,
+		'couleur_id' => $_POST['couleur'],
+		'nom' => $_POST['nom'],
+		'pays' => $_POST['pays'],
+		'region' => $_POST['region'],
+		'cepage' => $_POST['cepage']
+	));
+	$_SESSION['message'] = "Bouteille ajoutée !";
+}
 header('Location: index.php');
--- update_post.php
+++ update_post.php
@@ -12,62 +12,64 @@ if (isset($_POST['id']) && isset($_FILES)) {
         $string = mb_strtolower($string);
         return mb_strtoupper(mb_substr($string, 0, 1)) . mb_substr($string, 1);
     }
-    var_dump($_POST);
-    $id = intval($_POST['id']);
-    $nom = htmlentities(mb_strtoupper(trim($_POST['nom'])), ENT_QUOTES);
-    $pays = htmlentities(mb_strtoupper(trim($_POST['pays'])), ENT_QUOTES);
-    $region = htmlentities(mb_strtoupper(trim($_POST['region'])), ENT_QUOTES);
-    $cepage = htmlentities(trim($_POST['cepage']), ENT_QUOTES);
-    $annee = intval($_POST['annee']);
-    $description = htmlentities(mb_strtoupper(trim($_POST['description'])), ENT_QUOTES);
-    $_FILES['image']['name'];
-    $_FILES['image']['type'];
-    $_FILES['image']['size'];
-    $_FILES['image']['tmp_name'];
-    $_FILES['image']['error'];
-    $extensions = array('.png', '.gif', '.jpg', '.jpeg');
-    $extension = strrchr($_FILES['image']['name'], '.');
-    if (($_FILES['image']['size'] > 0) && ($_FILES['image']['size'] < 1000000) && (in_array($extension, $extensions))) {
-        $image = uniqid() . '.' . $_FILES['image']['name'];
-        $image = strtr(
-            $image,
-            'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
-            'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy'
-        );
-        $image = preg_replace('/([^.a-z0-9]+)/i', '-', $image);
-        $image = explode('.', $image);
-        $nom_image = $image['1'];
-        $image = $image['1'] . '.' . $image['2'];
-        $resultat = move_uploaded_file($_FILES['image']['tmp_name'], './assets/img/' . $image);
+
+    if (empty($_POST['nom'])) {
+        $_SESSION['erreur'] = "Merci de remplir le formulaire entièrement";
     } else {
-        $resultat = FALSE;
-    }
-    $sql_file = $resultat ? ', image=:image' : '';
-    $sql = "UPDATE mille 
+        $id = intval($_POST['id']);
+        $nom = htmlentities(mb_strtoupper(trim($_POST['nom'])), ENT_QUOTES);
+        $pays = htmlentities(mb_strtoupper(trim($_POST['pays'])), ENT_QUOTES);
+        $region = htmlentities(mb_strtoupper(trim($_POST['region'])), ENT_QUOTES);
+        $cepage = htmlentities(trim($_POST['cepage']), ENT_QUOTES);
+        $annee = intval($_POST['annee']);
+        $description = htmlentities(mb_strtoupper(trim($_POST['description'])), ENT_QUOTES);
+        $_FILES['image']['name'];
+        $_FILES['image']['type'];
+        $_FILES['image']['size'];
+        $_FILES['image']['tmp_name'];
+        $_FILES['image']['error'];
+        $extensions = array('.png', '.gif', '.jpg', '.jpeg');
+        $extension = strrchr($_FILES['image']['name'], '.');
+        if (($_FILES['image']['size'] > 0) && ($_FILES['image']['size'] < 1000000) && (in_array($extension, $extensions))) {
+            $image = uniqid() . '.' . $_FILES['image']['name'];
+            $image = strtr(
+                $image,
+                'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
+                'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy'
+            );
+            $image = preg_replace('/([^.a-z0-9]+)/i', '-', $image);
+            $image = explode('.', $image);
+            $nom_image = $image['1'];
+            $image = $image['1'] . '.' . $image['2'];
+            $resultat = move_uploaded_file($_FILES['image']['tmp_name'], './assets/img/' . $image);
+        } else {
+            $resultat = FALSE;
+        }
+        $sql_file = $resultat ? ', image=:image' : '';
+        $sql = "UPDATE mille 
         SET annee=:annee, description=:description, nom=:nom, pays=:pays, region=:region, cepage=:cepage$sql_file
         WHERE id=:id";
 
-    if ($req = $db->prepare($sql)) {
-        $req->bindParam(":nom", $nom, PDO::PARAM_STR);
-        $req->bindParam(":annee", $annee, PDO::PARAM_INT);
-        $req->bindParam(":pays", $pays, PDO::PARAM_STR);
-        $req->bindParam(":region", $region, PDO::PARAM_STR);
-        $req->bindParam(":cepage", $cepage, PDO::PARAM_STR);
-        $req->bindParam(":description", $description, PDO::PARAM_STR);
-        $req->bindParam(":id", $id, PDO::PARAM_INT);
-        if ($sql_file) {
-            $req->bindParam(":image", $image, PDO::PARAM_STR);
-        }
+        if ($req = $db->prepare($sql)) {
+            $req->bindParam(":nom", $nom, PDO::PARAM_STR);
+            $req->bindParam(":annee", $annee, PDO::PARAM_INT);
+            $req->bindParam(":pays", $pays, PDO::PARAM_STR);
+            $req->bindParam(":region", $region, PDO::PARAM_STR);
+            $req->bindParam(":cepage", $cepage, PDO::PARAM_STR);
+            $req->bindParam(":description", $description, PDO::PARAM_STR);
+            $req->bindParam(":id", $id, PDO::PARAM_INT);
+            if ($sql_file) {
+                $req->bindParam(":image", $image, PDO::PARAM_STR);
+            }
 
-        if ($req->execute()) {
-            $_SESSION['message'] = "Bouteille modifié !";
-            header('Location: index.php');
-        } else {
-            $_SESSION['erreur'] = "Un problème est survenu !";
-            header('Location: index.php');
+            if ($req->execute()) {
+                $_SESSION['message'] = "Bouteille modifié !";
+                header('Location: index.php');
+            } else {
+                $_SESSION['erreur'] = "Un problème est survenu !";
+                header('Location: index.php');
+            }
         }
     }
-} else {
-    $_SESSION['erreur'] = "Un problème est survenu !";
-    header('Location: index.php');
 }
+header('Location: index.php');
